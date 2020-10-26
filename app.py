61f737cb7c4892841d172d1e907e00b82b084dd5
agustin.albinati <agustin.albinati@accenture.com>
2020-10-26 16:57:01 -0300

    Changes on session id handler

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import streamlit.ReportThread as ReportThread
-from streamlit.server.Server import Server
+from streamlit.report_thread import get_report_ctx
 import pandas as pd
 import pickle
 
@@ -1257,34 +1256,8 @@ def checkLast(full_table):
 def get_session_id():
     # Hack to get the session object from Streamlit.
 
-    ctx = ReportThread.get_report_ctx()
-
-    this_session = None
-
-    current_server = Server.get_current()
-    if hasattr(current_server, '_session_infos'):
-        # Streamlit < 0.56
-        session_infos = Server.get_current()._session_infos.values()
-    else:
-        session_infos = Server.get_current()._session_info_by_id.values()
-
-    for session_info in session_infos:
-        s = session_info.session
-        if (
-                # Streamlit < 0.54.0
-                (hasattr(s, '_main_dg') and s._main_dg == ctx.main_dg)
-                or
-                # Streamlit >= 0.54.0
-                (not hasattr(s, '_main_dg') and s.enqueue == ctx.enqueue)
-        ):
-            this_session = s
-
-    if this_session is None:
-        raise RuntimeError(
-            "Oh noes. Couldn't get your Streamlit Session object"
-            'Are you doing something fancy with threads?')
-
-    return id(this_session)
+    ctx = get_report_ctx()
+    return ctx.session_id
 
 if __name__ == "__main__":
     main()
